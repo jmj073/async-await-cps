53b9d02fecf834f3f242e18bd3b8d39ef2e220e4
jmj073 <wjsalswns0733@gmail.com>
2026-01-25 19:07:01 +0900

    factorial 추가

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ class Future:
         return self.__resolved
 
 
-def my_async(func):
+def my_async(func, *args):
     fut = Future()
-    func(fut.resolve)
+    func(*args, fut.resolve)
     return fut
 
 def my_await(fut, k):
@@ -51,11 +51,35 @@ def foo(greet, k):
         k(f"{greet}, {v}")
     my_await(my_input(), k1)
 
+"""
+async fac(n):
+    print("enter!", n)
+    if n == 0:
+        return 1
+    v = await fac(n - 1)
+    print("await!", n)
+    return n * v
+"""
+def fac(n, k):
+    print("enter!", n)
+    if n == 0:
+        k(1)
+        return
+    def k1(v):
+        print("await!", n)
+        k(n * v)
+    my_await(my_async(fac, n - 1), k1)
+
 if __name__ == "__main__":
-    fut = my_async(lambda k: foo("Hello", k))
+    fut = my_async(foo, "Hello")
 
     fut.then(lambda v: print("resolved!", v))
     print("resolved: ", fut.is_resolved())
     print("resolve input")
     input_fut.resolve("world")
     print("resolved: ", fut.is_resolved())
+
+    print("-" * 50)
+
+    fut = my_async(fac, 5)
+    fut.then(lambda v: print("resolved!", v))
